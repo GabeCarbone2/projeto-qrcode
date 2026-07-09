b7f25cb1441ec50cb6732d2781871c6ec9542220
GabeCarbone2 <carbonegabriel913@gmail.com>
2026-07-08 22:29:10 -0300

    Gerando o qr-code

---
+++ src/prompts/prompt-qrcode.js
@@ -0,0 +1,17 @@
+import chalk from "chalk"
+
+const promptQRCode = [
+    {
+        name: "link",
+        description: chalk.yellow.bold("Digite o link para gerar o QRCode"),
+    },
+    {
+        name: "type",
+        description: chalk.yellow("escolha entre o tipo (1- NORMAL ou (2- Terminal "), 
+        pattern: /^[1-2]+$/, 
+        message: chalk.red.italic("Escolha apenas entre 1 e 2"), 
+        required: true,
+    }
+];
+
+export default promptQRCode
